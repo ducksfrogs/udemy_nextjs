0897d43c0136bd34fafba811b43acf7bc6198513
ducksfrogs <ma_yamaki@yahoo.com>
2024-07-08 13:28:34 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './compoment/Modal';
-import './compoment/UserCard';
+import Post from './compoment/Post';
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>hello 2</h1> 
-    </>
-  )
+  return <Post />;
 }
 
 export default App
---
+++ src/compoment/Post.jsx
@@ -0,0 +1,11 @@
+function Post() {
+    return (<div>
+        <p>Maximuman</p>
+        <p>react js is awsome</p>
+        <h1>ddd</h1>
+    </div>
+    );
+
+}
+
+export default Post;
