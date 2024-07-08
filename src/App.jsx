7f102b72e0e72251027f4fbaaf6af2635ff36f6e
ducksfrogs <ma_yamaki@yahoo.com>
2024-07-08 13:00:23 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './compoment/Modal';
+import './compoment/UserCard';
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>hello 2</h1> 
+    </>
+  )
+}
+
+export default App
