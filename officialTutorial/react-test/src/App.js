05ba879f6002bcf2502a63f2c8672f6dea611308
ducksfrogs <ma_yamaki@yahoo.com>
2024-10-23 06:16:19 +0900

    d

--- officialTutorial/react-test/src/App.js
+++ officialTutorial/react-test/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+    <hi> { user.name}</hi>
+    <img
+      className='avatar'
+      src={user.imageUrl}
+      alt={'Photo of' + user.name}
+      style={{
+        width: user.imageSize,
+        hight: user.imageSize
+      }}
+      />
+    </>
+  );
+}
+// import { useState } from 'react';
+
+// export default function App() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick(){
+//     setCount(count+1);
+//   }
+
+//   return(
+//     <div>
+//     <h1>Welcome to my app</h1>
+//     <button onClick={handleClick}>
+//       You pressed me { count } times.
+//     </button></div>
+//   )
+
+// }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
