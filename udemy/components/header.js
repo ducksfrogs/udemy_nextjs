f57c64196a54ba48e883fc36e2696f8e3fe4645c
ducksfrogs <ma_yamaki@yahoo.com>
2024-10-24 07:55:35 +0900

    d

---
+++ udemy/components/header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+            <p>Hit is header js</p>
+
+            <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+            <h1>Welcome to this NextJS Course!</h1>
+        </>
+    )
+}
