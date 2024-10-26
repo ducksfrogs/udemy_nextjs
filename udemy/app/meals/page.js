760177a7c064f79c876e5106b8deb8a906074a6f
ducksfrogs <ma_yamaki@yahoo.com>
2024-10-26 11:46:03 +0900

    d

--- udemy/app/meals/page.js
+++ udemy/app/meals/page.js
@@ -1,4 +1,26 @@
+import Link from 'next/link'
+import classes from './page.module.css'
+import MealsGrid from '@/components/meals/meals-grid';
+
+
 export default function MealsPage() {
-    console.log("meals page");
-    return <h1>Meals Page</h1>
-}
+    return (
+ <>
+    <header className={classes.header}>
+        <h1>
+            Delicious meals, created{' '}
+            <span className={classes.highligh}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself.</p>
+        <p className={classes.cta} >
+            <Link href={"/meals/share"}>
+            share your favorite recipe</Link>
+        </p>
+    </header>
+
+    <main className={classes.main} >
+        <MealsGrid />
+    </main>
+    </>
+
+    )}
