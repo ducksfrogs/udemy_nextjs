182dd11251779be8e70ef496744dd30cb0b70791
ducksfrogs <ma_yamaki@yahoo.com>
2024-10-26 16:57:32 +0900

    d

--- udemy/app/meals/page.js
+++ udemy/app/meals/page.js
@@ -1,26 +1,29 @@
 import Link from 'next/link'
 import classes from './page.module.css'
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
 
-export default function MealsPage() {
+export default async function MealsPage() {
+    const meals = await getMeals();
+
     return (
- <>
-    <header className={classes.header}>
-        <h1>
-            Delicious meals, created{' '}
-            <span className={classes.highligh}>by you</span>
-        </h1>
-        <p>Choose your favorite recipe and cook it yourself.</p>
-        <p className={classes.cta} >
-            <Link href={"/meals/share"}>
-            share your favorite recipe</Link>
-        </p>
-    </header>
+    <>
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created{' '}
+                <span className={classes.highligh}>by you</span>
+            </h1>
+            <p>Choose your favorite recipe and cook it yourself.</p>
+            <p className={classes.cta} >
+                <Link href={"/meals/share"}>
+                share your favorite recipe</Link>
+            </p>
+        </header>
 
-    <main className={classes.main} >
-        <MealsGrid />
-    </main>
+        <main className={classes.main} >
+            <MealsGrid meals={[meals]} />
+        </main>
     </>
 
     )}
