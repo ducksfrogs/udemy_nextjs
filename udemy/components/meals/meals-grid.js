d18b4f1413172976aafe50f308762619e224e8cc
ducksfrogs <ma_yamaki@yahoo.com>
2024-10-26 17:59:53 +0900

    d

--- udemy/components/meals/meals-grid.js
+++ udemy/components/meals/meals-grid.js
@@ -1,13 +1,14 @@
 import MealItem from './meal-item';
 import classes from './meals-grid.module.css';
 
-export default function MealsGrid({ meals }) {
+export default function MealsGrid( {meals} ) {
     return (<ul className={classes.meals}>
-        {meals ? meals.map((meal) => (<li key={meal.id}>
-            <MealItem {...meal} />
-        </li>
-        )) : null}
-
+     
+        {meals.map((meal) => (
+            <li key={meal.id}>
+                <MealItem {...meals} />
+            </li>
+        ))}
         </ul>
     );
 }
