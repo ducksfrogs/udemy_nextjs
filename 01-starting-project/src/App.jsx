bfb4513da25ca300f19c34f0a9b03e6d60567e49
ducksfrogs <ma_yamaki@yahoo.com>
2024-10-27 15:32:28 +0900

    d

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
+
 
 const reactDescriptions = ['Fundamental', 'Cruial', "Core"];
 
@@ -23,17 +24,27 @@ function Header () {
   );
 }
 
-function CoreConcept(props) {
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={ props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+
+//     </li>
+//   )
+// }
+
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={ props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={ title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
 
     </li>
   )
 }
-
 function App() {
   return (
     <div>
@@ -43,11 +54,22 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept 
-                title="Components"
-                description="The core UI building block."
-                image={ componentsImg }
+                title={CORE_CONCEPTS[0].title}
+                description={CORE_CONCEPTS[0].description}
+                image={ CORE_CONCEPTS[0].image }
             />
 
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept 
+                title={CORE_CONCEPTS[2].title}
+                description={CORE_CONCEPTS[2].description}
+                image={ CORE_CONCEPTS[2].image }
+            />
+            <CoreConcept 
+                title={CORE_CONCEPTS[3].title}
+                description={CORE_CONCEPTS[3].description}
+                image={ CORE_CONCEPTS[3].image }
+            />
           </ul>
         </section>
 
