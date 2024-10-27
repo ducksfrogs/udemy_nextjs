907f40f933504d06cc7afbe3adce0f333b51727f
ducksfrogs <ma_yamaki@yahoo.com>
2024-10-27 18:16:36 +0900

    d

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,81 +1,50 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
-
-const reactDescriptions = ['Fundamental', 'Cruial', "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header () {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-
-        <h1>React Essentials</h1>
-        <p>
-          { description } React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
-
-// function CoreConcept(props) {
-//   return (
-//     <li>
-//       <img src={props.image} alt={ props.title} />
-//       <h3>{props.title}</h3>
-//       <p>{props.description}</p>
-
-//     </li>
-//   )
-// }
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={ title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-
-    </li>
-  )
-}
 function App() {
   return (
-    <div>
-      <Header />
-      <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept 
-                title={CORE_CONCEPTS[0].title}
-                description={CORE_CONCEPTS[0].description}
-                image={ CORE_CONCEPTS[0].image }
-            />
+      <div>
+        <Header />
+        <main>
+          <section id="core-concepts">
+            <h2>Core Concepts</h2>
+            <ul>
+              <CoreConcept 
+                  title={CORE_CONCEPTS[0].title}
+                  description={CORE_CONCEPTS[0].description}
+                  image={ CORE_CONCEPTS[0].image }
+              />
 
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept 
-                title={CORE_CONCEPTS[2].title}
-                description={CORE_CONCEPTS[2].description}
-                image={ CORE_CONCEPTS[2].image }
-            />
-            <CoreConcept 
-                title={CORE_CONCEPTS[3].title}
-                description={CORE_CONCEPTS[3].description}
-                image={ CORE_CONCEPTS[3].image }
-            />
-          </ul>
-        </section>
+              <CoreConcept {...CORE_CONCEPTS[1]} />
+              <CoreConcept 
+                  title={CORE_CONCEPTS[2].title}
+                  description={CORE_CONCEPTS[2].description}
+                  image={ CORE_CONCEPTS[2].image }
+              />
+              <CoreConcept 
+                  title={CORE_CONCEPTS[3].title}
+                  description={CORE_CONCEPTS[3].description}
+                  image={ CORE_CONCEPTS[3].image }
+              />
+            </ul>
+          </section>
+          <section id="example">
+            <h2>Example</h2>
+            <menu>
+              <TabButton >Component</TabButton>
+              <TabButton >JSX</TabButton>
+              <TabButton >Props</TabButton>
+              <TabButton >Component</TabButton>
 
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
+            </menu>
+          </section>
+
+
+          <h2>Time to get started!</h2>
+        </main>
+      </div>
   );
 }
 
