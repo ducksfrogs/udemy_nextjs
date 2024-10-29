b84e6e7b6def8941893ada42498c5c78e86e1603
ducksfrogs <ma_yamaki@yahoo.com>
2024-10-29 15:01:16 +0900

    bigin second

---
+++ 07/src/App.jsx
@@ -0,0 +1,33 @@
+import Header from "./components/Header/Header"
+import Player from "./components/Player";
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <main>
+        <div id="game-container">
+          <ol id='players'>
+            <Player initialName='Player 1' symbol={"X"} />
+            <Player initialName='Player 2' symbol={"O"} />
+          </ol>
+      </div>
+    </main> 
+    </>
+  );
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 07/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header () {
+    return (
+        <>
+            <header>
+                <img src="../../public/game-logo.png" alt="" />
+                <h1>Tic tac toe</h1>
+            </header>
+        </>
+    )
+}
