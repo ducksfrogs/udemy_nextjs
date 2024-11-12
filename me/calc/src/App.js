6d5b690b30e0e9282c0e04413243e85def62455a
ducksfrogs <ma_yamaki@yahoo.com>
2024-11-12 10:34:23 +0900

    learning

--- me/calc/src/App.js
+++ me/calc/src/App.js
@@ -3,20 +3,33 @@ import Screen from "./components/Screen.js";
 import ButtonBox from "./components/ButtonBox.js";
 import Button from "./components/Button.js";
 
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
 function App() {
   return (
     <Wrapper>
-      <Screen value={"0"} />
+      <Screen value={0} />
       <ButtonBox>
-        <Button
-          className=""
-          value="0"
-          onClick={() => {
-
-          
-            console.log("Button clicked");
-          }}
-          />
+        {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })
+        }
       </ButtonBox>
     </Wrapper>
   );
